b38e524cee6fedba03fd3c47e03d12569e74023f
GBOHOUILI <eldomoreogbohouili@gmail.com>
2026-01-11 09:35:29 +0100

    Fix protect import in reservation routes svd

--- src/routes/reservationRoutes.js
+++ src/routes/reservationRoutes.js
@@ -6,20 +6,18 @@ import {
   deleteReservation,
   getReservationStats,
 } from "../controllers/reservationController.js";
-import { protect, restrictTo } from "../controllers/authController.js";
+
+import { protect } from "../middlewares/protect.js";
 
 const router = express.Router();
 
 // Protéger toutes les routes
 router.use(protect);
-router.use(restrictTo("admin"));
 
-router.route("/").get(getAllReservations);
-
-router.route("/stats").get(getReservationStats);
-
-router.route("/:id").get(getReservationById).delete(deleteReservation);
-
-router.route("/:id/status").patch(updateReservationStatus);
+router.get("/", getAllReservations);
+router.get("/stats", getReservationStats);
+router.get("/:id", getReservationById);
+router.delete("/:id", deleteReservation);
+router.patch("/:id/status", updateReservationStatus);
 
 export default router;
